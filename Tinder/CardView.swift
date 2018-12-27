add468eb9d53cb29373a5c5f3a2a7ea7c4eb8489
Owen Henley <owenhenley@me.com>
2018-12-27 17:30:38 -0500

    Swipe gestures implemented
    needs some refactoring

--- Tinder/CardView.swift
+++ Tinder/CardView.swift
@@ -11,6 +11,7 @@ import UIKit
 class CardView: UIView {
     
     fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    fileprivate let threshhold: CGFloat = 100
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,7 +32,7 @@ class CardView: UIView {
         case .changed:
             handleChanged(gesture)
         case.ended:
-            handleEnded()
+            handleEnded(gesture)
         default:
             ()
         }
@@ -39,13 +40,28 @@ class CardView: UIView {
     
     fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: nil)
-        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        // convert Radians to Degrees
+        let degrees: CGFloat = translation.x / 30
+        let angle = degrees * .pi / 100
+        let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
     }
     
-    fileprivate func handleEnded() {
-        UIView.animate(withDuration: 0.65, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseInOut, animations: {
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+        // set card movement threshold for bool management
+        let translationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
+        let shouldDismissCard = abs(gesture.translation(in: nil).x) > threshhold
+        
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseInOut, animations: {
+            if shouldDismissCard {
+                self.frame = CGRect(x: 1000 * translationDirection, y: 0, width: self.frame.width, height: self.frame.height)
+            } else {
+                self.transform = .identity
+            }
+        }) { (newCard) in
             self.transform = .identity
-        })
+            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
+        }
     }
     
     
